cb02e7eb6328278122c414695d6bc0b7f8a8b035
tree2017 <bluehighwayjp@yahoo.co.jp>
2018-04-11 02:11:24 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // String line = br.readLine();
+    
+        String str1 = null;
+        System.out.println(str1.length());
+    }
+}
